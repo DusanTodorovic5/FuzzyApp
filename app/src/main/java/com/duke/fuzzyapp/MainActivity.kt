a7ec8d25d1b6d9eecc2a51e9b2567e8fdfe7c66a
DusanTodorovic5 <dtodorovic40@gmail.com>
2022-02-11 16:48:45 +0100

    test commit

--- app/src/main/java/com/duke/fuzzyapp/MainActivity.kt
+++ app/src/main/java/com/duke/fuzzyapp/MainActivity.kt
@@ -29,8 +29,8 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            Snackbar.make(view, "Moja akcija", Snackbar.LENGTH_LONG)
+                .setAction("AKCIJA", null).show()
         }
     }
 
