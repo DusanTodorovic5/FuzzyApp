656e353a1fc9527bad638e8711a2f6df3563a34f
Dušan Todorović <79877752+DusanTodorovic5@users.noreply.github.com>
2022-03-13 15:16:53 +0100

    Update Main.java

--- lib/Main.java
+++ lib/Main.java
@@ -25,10 +25,15 @@ public class Main {
 			out.getVariable().setStart(20);
 			MembershipFunction of11 = new TrapezoidMembershipFunction();
 			of11.addParameters(20, 0);
-			of11.addParameters(40, 1);
-			of11.addParameters(60, 2);
-			of11.addParameters(80, 3);
+			of11.addParameters(30, 1);
+			of11.addParameters(40, 2);
+			of11.addParameters(50, 3);
 			out.addMembershipFunction(of11);
+			MembershipFunction of12 = new TriangleMembershipFunction();
+			of12.addParameters(60, 0);
+			of12.addParameters(70, 1);
+			of12.addParameters(90, 2);
+			out.addMembershipFunction(of12);
 			
 			mojSistem.addInput(fv);
 			mojSistem.addInput(fv2);
@@ -48,11 +53,11 @@ public class Main {
 			MembershipFunction mf11 = new TrapezoidMembershipFunction();
 			mf11.addParameters(0, 0);
 			mf11.addParameters(2, 1);
-			mf11.addParameters(4, 2);
-			mf11.addParameters(8, 3);
+			mf11.addParameters(3, 2);
+			mf11.addParameters(4, 3);
 			MembershipFunction mf12 = new TriangleMembershipFunction();
-			mf12.addParameters(3, 0);
-			mf12.addParameters(5, 1);
+			mf12.addParameters(5, 0);
+			mf12.addParameters(7, 1);
 			mf12.addParameters(9, 2);
 			fvs.get(0).addMembershipFunction(mf11);
 			fvs.get(0).addMembershipFunction(mf12);
@@ -67,6 +72,12 @@ public class Main {
 			mf21.addParameters(4, 2);
 			mf21.addParameters(9, 3);
 			fvs.get(1).addMembershipFunction(mf21);
+			MembershipFunction mf22 = new TrapezoidMembershipFunction();
+			mf22.addParameters(10, 0);
+			mf22.addParameters(13, 1);
+			mf22.addParameters(17, 2);
+			mf22.addParameters(19, 3);
+			fvs.get(1).addMembershipFunction(mf22);
 			
 			System.out.println("OUTPUTS");
 			ArrayList<Output> outs = mojSistem.getOutputs();
@@ -82,9 +93,15 @@ public class Main {
 			rule.addOutput(new Triple<Output,Integer,Boolean>(out,0,false));
 			mojSistem.addRule(rule);
 			
+			Rule rule2 = new Rule();
+			rule2.addInput(new Triple<FuzzyVariable,Integer,Boolean>(fv,0,false));
+			rule2.addInput(new Triple<FuzzyVariable,Integer,Boolean>(fv2,1,false));
+			rule2.addOutput(new Triple<Output,Integer,Boolean>(out,1,false));
+			mojSistem.addRule(rule2);
+			
 			HashMap<String, Double> mapa = new HashMap<String,Double>();
-			mapa.put("FV Test",4.0);
-			mapa.put("FV2 Test",5.0);
+			mapa.put("FV Test",1.0);
+			mapa.put("FV2 Test",18.0);
 			
 			System.out.println("###################");
 			HashMap<String,Double> mapica = mojSistem.evaluate(mapa);
